3c6512eb27e7e54796371d611e44e8428447fc80
radhasahu916 <radhasahu5657@gmail.com>
2025-09-12 13:08:35 +0530

    Add files via upload

---
+++ fed/11aug/aug1111.c/Untitled18.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+   int  a;
+   int b;
+   int c;
+
+     printf("enter the side of trangle");
+     scanf("%d",&a);
+     printf("enter the side of trangle");
+     scanf("%d",&b);
+     printf("enter the side of trangle");
+     scanf("%d",&c);
+     if(a==b){
+     }if(b==c){
+        printf("equal");
+
+     }else if(a==b||b==c||a==c);{
+     printf("isoleles");}
+}
+
